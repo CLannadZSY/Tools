fdc9181d9e165e8671e2a316ffb5a71263fe5522
CLannadZSY <zsymidi@gmail.com>
2020-03-28 14:53:51 +0800

    全新写法, 配置更加合理与自由

--- db_utils/db_conf.py
+++ db_utils/db_conf.py
@@ -1,27 +1,29 @@
 
-# [redis] 连接配置
-# 推荐使用 127 因为使用localhost 会有2秒的延时
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
-REDIS_PASSWORD = ''
-REDIS_MAX_CONN = 100
-# 这样也不失为一种好办法,待定使用
-# REDIS_CONFIG = {
-#     'host': "",
-#     'host': "",
-#     'host': "",
-#     'host': "",
-#     'host': "",
-#     'host': "",
-# }
-
-# [mysql] 连接配置
-MYSQL_CONFIG = {
-    "host": "localhost",
-    "port": 3306,
-    "user": "root",
-    "password": "xxx",
+# 开发环境
+MYSQL_CONFIG_DEV = {
+    'db_name': {
+        "host": "host_ip",
+        "port": 'port',
+        "user": "username",
+        "password": "password",
+        "database": 'db_name'
+    },
 }
+
+
+# 线上环境
+MYSQL_CONFIG_PROD = {
+    'db_name': {
+        "host": "host_ip",
+        "port": 'port',
+        "user": "username",
+        "password": "password",
+        "database": 'db_name'
+    },
+
+}
+
+
 
 
 
