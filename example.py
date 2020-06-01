e0e9cfd21d83173c6e2c8e862a8e5776d6aa90f3
CLannadZSY <zsymidi@gmail.com>
2020-06-01 20:51:09 +0800

    添加订阅模式的使用案例

--- example.py
+++ example.py
@@ -14,6 +14,24 @@ from redis_utils.redis_conf import REDIS_CONFIG_DEV, REDIS_CONFIG_PROD
 
 r_0 = RedisConnPool(REDIS_CONFIG_DEV['redis_name']).connect()
 
+# [redis]发布
+r_0 = RedisConnPool(REDIS_CONFIG_DEV['redis_name']).connect()
+chan_sub = 'channel_name'
+message_content = 'Hello World'
+r_0.publish(chan_sub, message_content)
+
+# [redis]订阅
+channel_sub = 'channel_name'
+redis_conn = RedisConnPool(REDIS_CONFIG_PROD['redis_name'])
+redis_sub = redis_conn.subscribe(channel_sub)
+r_0 = redis_conn.connect()
+while True:
+    msg = redis_sub.listen()
+    for i in msg:
+        if i["type"] == "message" and i['channel'] == channel_sub:
+            print(f'接收到消息: {i["data"]=}')
+
+
 # [kafka]
 # 详细参考: https://github.com/confluentinc/confluent-kafka-python
 from kafka_utils.kafka_tool import ConfluentKafkaProducer, ConfluentKafkaConsumer
