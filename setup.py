a61987327e4e7ac05b9e7dd2718d3236b1646f4f
CLannadZSY <zsymidi@gmail.com>
2020-10-20 22:36:50 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Py-library",
+    version="1.0.4",
+    author="CLannadZSY",
+    author_email="zsymidi@gmail.com",
+    description="py 工具箱",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CLannadZSY/Tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    py_modules=["Py-library"],
+)
