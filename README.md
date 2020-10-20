25ecfc97a5e2396255bb38a456aeef26b57548ea
CLannadZSY <zsymidi@gmail.com>
2020-10-20 22:55:21 +0800

    发布py_library后, 更新 readme和example的使用文档

--- README.md
+++ README.md
@@ -22,54 +22,6 @@ pip install redis pymysql
 ```
 
 ### Usage example [使用示例](example.py)
-* 使用前, 请修改 ```*.conf.py``` 中的配置, 确保配置正确
-```python
-# [redis]
-from redis_utils.redis_conn import RedisConnPool
-from redis_utils.redis_conf import REDIS_CONFIG_DEV, REDIS_CONFIG_PROD
-
-r_0 = RedisConnPool(REDIS_CONFIG_DEV['redis_name']).connect()
-```
-
-```python
-# [redis]发布
-from redis_utils.redis_conn import RedisConnPool
-from redis_utils.redis_conf import REDIS_CONFIG_DEV, REDIS_CONFIG_PROD
-r_0 = RedisConnPool(REDIS_CONFIG_DEV['redis_name']).connect()
-chan_sub = 'channel_name'
-message_content = 'Hello World'
-r_0.publish(chan_sub, message_content)
-
-# [redis]订阅
-channel_sub = 'channel_name'
-redis_conn = RedisConnPool(REDIS_CONFIG_PROD['redis_name'])
-redis_sub = redis_conn.subscribe(channel_sub)
-r_0 = redis_conn.connect()
-while True:
-    msg = redis_sub.listen()
-    for i in msg:
-        if i["type"] == "message" and i['channel'] == channel_sub:
-            print(f'接收到消息: {i["data"]=}')
-```
-
-```python
-# [mysql]
-from mysql_utils.mysql_conn import MysqlPooledDB
-from mysql_utils.mysql_conf import MYSQL_CONFIG_DEV, MYSQL_CONFIG_PROD
-
-conn, cursor = MysqlPooledDB(MYSQL_CONFIG_DEV['db_name']).connect()
-```
-
-```python
-# [kafka]
-# 详细参考: https://github.com/confluentinc/confluent-kafka-python
- 
-from kafka_utils.kafka_tool import ConfluentKafkaProducer, ConfluentKafkaConsumer
-
-producer = ConfluentKafkaProducer().producer()
-consumer = ConfluentKafkaConsumer().consumer()
-```
-
 
 ## Contributing 贡献指南
 
--- example.py
+++ example.py
@@ -3,15 +3,29 @@
 """
 
 # [mysql]
-from mysql_utils.mysql_conn import MysqlPooledDB
-from mysql_utils.mysql_conf import MYSQL_CONFIG_DEV, MYSQL_CONFIG_PROD
-
+from py_library.mysql_conn import MysqlPooledDB
+MYSQL_CONFIG_DEV = {
+    '1bom.1bomSpider': {
+        "host": "127.0.0.1",
+        "port": 3306,
+        "user": "username",
+        "password": "password",
+        "database": 'dbname'
+    },
+}
 conn, cursor = MysqlPooledDB(MYSQL_CONFIG_DEV['db_name']).connect()
 
 # [redis]
-from redis_utils.redis_conn import RedisConnPool
-from redis_utils.redis_conf import REDIS_CONFIG_DEV, REDIS_CONFIG_PROD
-
+from py_library.redis_conn import RedisConnPool
+REDIS_CONFIG_DEV = {
+    'redis_name': {
+        'host': "127.0.0.1",
+        'port': 6379,
+        'password': '',
+        'max_connections': 100,
+        'db': 0
+    },
+}
 r_0 = RedisConnPool(REDIS_CONFIG_DEV['redis_name']).connect()
 
 # [redis]发布
@@ -22,7 +36,7 @@ r_0.publish(chan_sub, message_content)
 
 # [redis]订阅
 channel_sub = 'channel_name'
-redis_conn = RedisConnPool(REDIS_CONFIG_PROD['redis_name'])
+redis_conn = RedisConnPool(REDIS_CONFIG_DEV['redis_name'])
 redis_sub = redis_conn.subscribe(channel_sub)
 r_0 = redis_conn.connect()
 while True:
@@ -34,7 +48,17 @@ while True:
 
 # [kafka]
 # 详细参考: https://github.com/confluentinc/confluent-kafka-python
-from kafka_utils.kafka_tool import ConfluentKafkaProducer, ConfluentKafkaConsumer
+from py_library.kafka_conn import ConfluentKafkaProducer, ConfluentKafkaConsumer
 
-producer = ConfluentKafkaProducer().producer()
-consumer = ConfluentKafkaConsumer().consumer()
+DEV_CONFLUENT_PRODUCER_CONFIG = {
+    'bootstrap.servers': 'server_addr',
+}
+
+DEV_CONFLUENT_CONSUMER_CONFIG = {
+    'bootstrap.servers': 'server_addr',
+    'group.id': 'group_name',
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': True
+}
+producer = ConfluentKafkaProducer(DEV_CONFLUENT_PRODUCER_CONFIG).producer()
+consumer = ConfluentKafkaConsumer(DEV_CONFLUENT_CONSUMER_CONFIG).consumer()
