a0169113e88bb6b6a9aa85a8962e28500efd01de
CLannadZSY <zsymidi@gmail.com>
2020-10-22 20:56:46 +0800

    添加redis Flyweight Pattern模式, 可避免重复创建连接,

--- py_library/redis_conn.py
+++ py_library/redis_conn.py
@@ -37,3 +37,26 @@ class RedisConnPool(object):
         # 超过最大连接数,使用这个关闭,但并不会阻止打开新的连接
         self._pool.disconnect()
         self._r.close()
+
+
+# from pypattyrn.structural.flyweight import FlyweightMeta
+# from redis import Redis
+#
+#
+# class RedisConn(Redis, metaclass=FlyweightMeta):
+#     """
+#     redis连接享元模式无需担心重复创建连接
+#     """
+#     pass
+#
+#
+# if __name__ == '__main__':
+#     redis_host = '127.0.0.1'
+#     redis_port = 6379
+#     redis_db = 0
+#     password = ''
+#     redis1 = RedisConn(host=redis_host, port=redis_port, db=redis_db, password=password)
+#     redis2 = RedisConn(host=redis_host, port=redis_port, db=redis_db, password=password)
+#     print(id(redis1) == id(redis2))
+#     redis3 = RedisConn(host=redis_host, port=redis_port, db=1, password=password)
+#     print(id(redis1) == id(redis3))
