2d2f89043d2d488d960676825b71f5804712a38b
clannadzsy <zsymidi@gmail.com>
2025-01-01 20:13:51 +0800

    update models.py

--- models.py
+++ models.py
@@ -1,14 +1,21 @@
-from sqlalchemy import *
-from sqlalchemy.orm import declarative_base
+from typing import Optional
 
-Base = declarative_base()
+from sqlalchemy import *
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 class Test1(Base):
     __tablename__ = 'test_1'
+    __table_args__ = (
+        Index('u_id', 'a', unique=True),
+    )
 
-    id = Column(Integer, primary_key=True)
-    a = Column(String(255, collation='utf8mb4_unicode_ci'))
-    b = Column(String(255, collation='utf8mb4_unicode_ci'))
-    c = Column(String(255, collation='utf8mb4_unicode_ci'))
-    d = Column(String(255, collation='utf8mb4_unicode_ci'))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    a: Mapped[Optional[int]] = mapped_column(Integer)
+    b: Mapped[Optional[int]] = mapped_column(Integer)
+    c: Mapped[Optional[int]] = mapped_column(Integer)
+    d: Mapped[Optional[int]] = mapped_column(Integer)
